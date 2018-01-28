116185b9041670a5c5bff94e214a0a3341a9e55a
twalke <tyler.walke@gmail.com>
2018-01-27 17:08:47 -0700

    fixed buttons.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function onAboutClick() {
-  $(".about").toggleClass("visible");
-}
+
+
+  function onAboutClick() {
+    $(".about").toggleClass("visible");
+    if($("#projects2").hasClass("visible")) {
+      $(".projects").toggleClass("visible");
+    }
+  }
+
+  function onProjectsClick() {
+    $(".projects").toggleClass("visible");
+    $(".about").toggleClass("visible");
+
+  }
