4639870c2950600c17a44dab186187cd9fd273e1
twalke <tyler.walke@gmail.com>
2018-01-31 23:02:05 -0700

    added space for resume length.

--- app.js
+++ app.js
@@ -43,3 +43,36 @@
       $(".resume").toggleClass("visible");
     }
   }
+
+//$("button").click(onAboutClick()) {
+//  $("#title").slideUp();
+//});
+
+//  $("button").click(onAboutClick()) {
+//    $("title").animate({left:'50px'});
+//  }
+
+
+
+  /*function nameHeight() {
+    var elem = document.getElementById("title");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+
+  }*/
+
+
+  /*if($("#about2").hasClass("visible")||$("#projects2").hasClass("visible")||$("#resume2").hasClass("visible")||$("#email2").hasClass("visible")) {
+    clearInterval(id);
+  } else {
+    $("#landingPage h1")margin-top: 30vh;
+  }*/
