64aa2949697acc25ca9d1c7ca5162b8a623a4603
twalke <tyler.walke@gmail.com>
2018-03-04 02:49:02 -0700

    Deleted unnecessary code.

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// function loadFunction() {
-//   $("#hide")
-// }
-
- $(document).ready(function() {
+$(document).ready(function() {
    $(".about").hide();
    $(".projects").hide();
    $("table").hide();
@@ -50,73 +46,6 @@ function onEmailClick() {
   $(".hobbies").hide();
 }
 
-
-
-  // function onAboutClick() {
-  //   $(".about").toggleClass("visible");
-  //   if($("#projects2").hasClass("visible")) {
-  //     $(".projects").toggleClass("visible");
-  //   } else if($("table").hasClass("visible")) {
-  //     $("table").toggleClass("visible");
-  //   } else if($("#email2").hasClass("visible")) {
-  //     $(".email").toggleClass("visible");
-  //   } else if($("#hobbies2").hasClass("visible")) {
-  //     $(".hobbies").toggleClass("visible");
-  //   }
-  // }
-  //
-  // function onProjectsClick() {
-  //   $(".projects").toggleClass("visible");
-  //   if($("#about2").hasClass("visible")) {
-  //     $(".about").toggleClass("visible");
-  //   } else if($("table").hasClass("visible")) {
-  //     $("table").toggleClass("visible");
-  //   } else if($("#email2").hasClass("visible")) {
-  //     $(".email").toggleClass("visible");
-  //   } else if($("#hobbies2").hasClass("visible")) {
-  //     $(".hobbies").toggleClass("visible");
-  //   }
-  // }
-  //
-  // function onResumeClick() {
-  //   $("table").toggleClass("visible");
-  //   if($("#about2").hasClass("visible")) {
-  //     $(".about").toggleClass("visible");
-  //   } else if($("#projects2").hasClass("visible")) {
-  //     $(".projects").toggleClass("visible");
-  //   } else if($("#email2").hasClass("visible")) {
-  //     $(".email").toggleClass("visible");
-  //   } else if($("#hobbies2").hasClass("visible")) {
-  //     $(".hobbies").toggleClass("visible");
-  //   }
-  // }
-
   function onDownloadClick() {
     window.open("Resume_TylerWalke_January2018_v5.pdf");
   }
-
-  // function onEmailClick() {
-  //   $(".email").toggleClass("visible");
-  //   if($("#about2").hasClass("visible")) {
-  //     $(".about").toggleClass("visible");
-  //   } else if($("#projects2").hasClass("visible")) {
-  //     $(".projects").toggleClass("visible");
-  //   } else if($("table").hasClass("visible")) {
-  //     $("table").toggleClass("visible");
-  //   } else if($("#hobbies2").hasClass("visible")) {
-  //     $(".hobbies").toggleClass("visible");
-  //   }
-  // }
-  //
-  // function onHobbiesClick() {
-  //   $(".hobbies").toggleClass("visible");
-  //   if($("#projects2").hasClass("visible")) {
-  //     $(".projects").toggleClass("visible");
-  //   } else if($("table").hasClass("visible")) {
-  //     $("table").toggleClass("visible");
-  //   } else if($("#email2").hasClass("visible")) {
-  //     $(".email").toggleClass("visible");
-  //   } else if($("#about2").hasClass("visible")) {
-  //     $(".about").toggleClass("visible");
-  //   }
-  // }
