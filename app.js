9d709d983c0aa76a7e8b7a1cd1adc4ebf593d899
Tyler Walke <tyler.walke@gmail.com>
2018-12-17 13:20:22 -0700

    3.0
    3.0

--- app.js
+++ app.js
@@ -1,118 +1,61 @@
-$(document).ready(function() {
-   $(".about").hide();
-   $(".projects").hide();
-   $(".resume").hide();
-   $(".hobbies").hide();
-   $(".email").hide();
-
-	 $("#menuwrap").click(function() {
-     $("#menu").addClass(function(index, currentClass) {
-       if (currentClass === "hid")
-         $("#menu").removeClass("hid").addClass("active");
-       else
-         $("#menu").removeClass("active").addClass("hid");
-     });
-     $("#menuwrap").addClass(function(index, currentClass) {
-       if (currentClass === "hid2")
-         $("#menuwrap").removeClass("hid2").addClass("active2");
-       else
-         $("#menuwrap").removeClass("active2").addClass("hid2");
-     });
-     $("#text").addClass(function(index, currentClass) {
-       if (currentClass === "hidtext")
-         $("#text").removeClass("hidtext").addClass("shotext");
-       else
-         $("#text").removeClass("shotext").addClass("hidtext");
-       });
-     })
-
-     function myFunction(x) {
-      if (x.matches) {
-        setTimeout(function(){
-          $("body").css("background-image", "url(tyler-paint-small.jpeg)");
-        }, 500);
-      } else {
-        setTimeout(function(){
-          $("body").css("background-image", "url(tyler-paint-left.jpeg)");
-        }, 500);
-      }
-    }
-
-    var x = window.matchMedia("(max-width: 700px)")
-    myFunction(x)
-    x.addListener(myFunction)
- });
-
-function onAboutClick() {
-  $(".about").show();
-  $(".projects").hide();
-  $(".resume").hide();
-  $(".hobbies").hide();
-  $(".email").hide();
-	$("#title").hide();
-	$("#subTitle").hide();
-	$("#menu").removeClass("active").addClass("hid");
-  $("#menuwrap").removeClass("active2").addClass("hid2");
-  $("#text").removeClass("shotext").addClass("hidtext");
-  $("body").css("background-image", "url(canvas.jpeg)");
-}
-
-function onResumeClick() {
-  $(".resume").show();
-  $(".projects").hide();
-  $(".about").hide();
-  $(".hobbies").hide();
-  $(".email").hide();
-	$("#title").hide();
-	$("#subTitle").hide();
-  $("#menu").removeClass("active").addClass("hid");
-  $("#menuwrap").removeClass("active2").addClass("hid2");
-  $("#text").removeClass("shotext").addClass("hidtext");
-	$("body").css("background-image", "url(canvas.jpeg)");
-}
-
-function onProjectsClick() {
-  $(".projects").show();
-  $(".about").hide();
-  $(".resume").hide();
-  $(".hobbies").hide();
-  $(".email").hide();
-	$("#title").hide();
-	$("#subTitle").hide();
-	$("#menu").removeClass("active").addClass("hid");
-  $("#menuwrap").removeClass("active2").addClass("hid2");
-  $("#text").removeClass("shotext").addClass("hidtext");
-	$("body").css("background-image", "url(canvas.jpeg)");
-}
-
-function onHobbiesClick() {
-  $(".hobbies").show();
-  $(".about").hide();
-  $(".projects").hide();
-  $(".resume").hide();
-  $(".email").hide();
-	$("#title").hide();
-	$("#subTitle").hide();
-	$("#menu").removeClass("active").addClass("hid");
-  $("#menuwrap").removeClass("active2").addClass("hid2");
-  $("#text").removeClass("shotext").addClass("hidtext");
-	$("body").css("background-image", "url(canvas.jpeg)");
-}
-
-function onEmailClick() {
-  $(".email").show();
-  $(".about").hide();
-  $(".projects").hide();
-  $(".resume").hide();
-  $(".hobbies").hide();
-	$("#title").hide();
-	$("#subTitle").hide();
-	$("#menu").removeClass("active").addClass("hid");
-  $("#menuwrap").removeClass("active2").addClass("hid2");
-  $("#text").removeClass("shotext").addClass("hidtext");
-	$("body").css("background-image", "url(canvas.jpeg)");
-}
+$("#aboutTag").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#aboutScreen").offset().top},
+        'slow');
+});
+$("#projectsTag").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#projectsScreen").offset().top},
+        'slow');
+});
+$("#resumeTag").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#resumeScreen").offset().top},
+        'slow');
+});
+$("#contactTag").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contactScreen").offset().top},
+        'slow');
+});
 
 function onDownloadClick() {
   window.open("Resume_Tyler_Walke.pdf");
 }
+
+
+//
+// var stickySidebar = $('#resumeTitle');
+//
+// if (stickySidebar.length > 0) {
+//   var stickyHeight = stickySidebar.height(),
+//       sidebarTop = stickySidebar.offset().top;
+// }
+//
+// // on scroll move the sidebar
+// $(window).scroll(function () {
+//   if (stickySidebar.length > 0) {
+//     var scrollTop = $(window).scrollTop();
+//
+//     if (sidebarTop < scrollTop) {
+//       stickySidebar.css('top', scrollTop - sidebarTop);
+//
+//       // stop the sticky sidebar at the footer to avoid overlapping
+//       var sidebarBottom = stickySidebar.offset().top + stickyHeight;
+//       var stickyStop = $('container').offset().top + $('container').height();
+//       if (stickyStop < sidebarBottom) {
+//         var stopPosition = $('container').height() - stickyHeight;
+//         stickySidebar.css('top', stopPosition);
+//       }
+//     }
+//     else {
+//       stickySidebar.css('top', '0');
+//     }
+//   }
+// });
+//
+// $(window).resize(function () {
+//   if (stickySidebar.length > 0) {
+//     stickyHeight = stickySidebar.height();
+//   }
+// });
